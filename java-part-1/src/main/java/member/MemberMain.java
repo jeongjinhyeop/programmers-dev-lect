a513426e026399ac0919559f0f57e3188f314c62
mac <mac@macui-MacBookPro.local>
2026-06-08 12:50:17 +0900

    도전과제 3번 까지 적용

--- java-part-1/src/main/java/member/MemberMain.java
+++ java-part-1/src/main/java/member/MemberMain.java
@@ -1,5 +1,7 @@
 package member;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class MemberMain {
@@ -12,7 +14,7 @@ public class MemberMain {
         System.out.println("[요금제를 선택하세요]");
         System.out.println("[1]Lite : 10 명 [2]Basic : 20 명 [3]Premium : 30 명");
 
-        return sc.nextInt();
+        return Integer.parseInt(sc.nextLine());
     }
 
     public static int printMenu(int memberCnt) {
@@ -22,13 +24,14 @@ public class MemberMain {
         System.out.println("[4]회원전체조회 [5]회원정보 수정 [6]회원삭제");
         System.out.println("[7]프로그램 종료");
 
-        return sc.nextInt();
+        return Integer.parseInt(sc.nextLine());
     }
 
-    public static void addMember(String[][] members) {
+    public static void addMember(List<Member> members) {
         System.out.println("이름을 입력해주세요.");
         String name = sc.nextLine();
-        if(memberCnt == members.length){
+        System.out.println("memberCOunt: " + memberCnt);
+        if(memberCnt == totalCnt){
             System.out.println("회원이 꽉 찼습니다.");
             return;
         }
@@ -43,25 +46,28 @@ public class MemberMain {
         System.out.println("핸드폰 번호를 입력해주세요.");
         String phone = sc.nextLine();
 
-        members[memberCnt][0] = name;
-        members[memberCnt][1] = email;
+        Member newMember = new Member(name, email, phone);
+        members.add(newMember);
+
+        /*members[memberCnt][1] = email;
         members[memberCnt][2] = phone;
-        memberCnt++;
+        memberCnt++;*/
+        memberCnt = members.size();
     }
 
-    public static boolean checkEmail(String[][] members, String email) {
-        for (int i = 0; i < members.length; i++) {
-            if(email.equals(members[i][1])) return true;
+    public static boolean checkEmail(List<Member> members, String email) {
+        for (int i = 0; i < members.size(); i++) {
+            if(email.equals(members.get(i).getEmail())) return true;
         }
         return false;
     }
 
-    public static void selectEmail(String[][] members, String email){
-        for (int i = 0; i < members.length; i++) {
-            if(email.equals(members[i][1])){
-                System.out.println("[이름] " + members[i][0] +
-                        ", [이메일] " + members[i][1] +
-                        ", [연락처] " + members[i][2]);
+    public static void selectEmail(List<Member> members, String email){
+        for (int i = 0; i < members.size(); i++) {
+            if(email.equals(members.get(i).getEmail())){
+                System.out.println("[이름] " + members.get(i).getName() +
+                        ", [이메일] " + members.get(i).getEmail() +
+                        ", [연락처] " + members.get(i).getPhone());
 
                 return;
             }
@@ -69,55 +75,64 @@ public class MemberMain {
         System.out.println("찾으시는 정보가 없습니다.");
     }
 
-    public static void selectName(String[][] members, String name){
-        for (int i = 0; i < members.length; i++) {
-            if(name.equals(members[i][0])){
-                System.out.println("[이름] " + members[i][0] +
-                        ", [이메일] " + members[i][1] +
-                        ", [연락처] " + members[i][2]);
+    public static void selectName(List<Member> members, String name){
+        boolean flag = false;
+        for (int i = 0; i < members.size(); i++) {
+            if(name.equals(members.get(i).getName())){
+                flag = true;
+                System.out.println("[이름] " + members.get(i).getName() +
+                        ", [이메일] " + members.get(i).getEmail() +
+                        ", [연락처] " + members.get(i).getPhone());
 
-                return;
             }
         }
-        System.out.println("찾으시는 정보가 없습니다.");
+
+        if(!flag) System.out.println("찾으시는 정보가 없습니다.");
     }
 
-    public static void selectAll(String[][] members){
+    public static void selectAll(List<Member> members){
         for (int i = 0; i < memberCnt; i++) {   // memberCnt 까지만! (빈 칸 null 출력 방지)
-            System.out.println("[이름] " + members[i][0] +
-                    ", [이메일] " + members[i][1] +
-                    ", [연락처] " + members[i][2]);
+            System.out.println("[이름] " + members.get(i).getName() +
+                    ", [이메일] " + members.get(i).getEmail() +
+                    ", [연락처] " + members.get(i).getPhone());
         }
         return;
     }
-    public static void updateMember(String[][] members, String email){
+    public static void updateMember(List<Member> members, String email){
         int idx = -1;
-        for (int i = 0; i < members.length; i++) {
-            if (email.equals(members[i][1])) {
+        for (int i = 0; i < members.size(); i++) {
+            if (email.equals(members.get(i).getEmail())) {
                 idx = i;
-                System.out.println("이름을 입력해주세요.");
-                members[i][0] = sc.nextLine();
-                System.out.println("이메일을 입력해주세요.");
-                members[i][1] = sc.nextLine();
-                System.out.println("번호를 입력해주세요.");
-                members[i][2] = sc.nextLine();
+                System.out.println("변경할 이름을 입력해주세요.");
+                members.get(i).setName(sc.nextLine());
+                System.out.println("변경할 이메일을 입력해주세요.");
+                members.get(i).setEmail(sc.nextLine());
+                System.out.println("변경할 번호를 입력해주세요.");
+                members.get(i).setPhone(sc.nextLine());
                 break;
             }
         }
         if (idx == -1) { System.out.println("찾으시는 회원이 없습니다."); return; }
     }
 
-    public static void deleteMember(String[][] members, String email){
+    public static void deleteMember(List<Member> members, String email){
 
-        for (int i = 0; i < members.length; i++) {
-            if(email.equals(members[i][1])){
-                members[i][0] = members[i + 1][0];
+        for (int i = 0; i < members.size(); i++) {
+            if(email.equals(members.get(i).getEmail())){
+                members.remove(i);
+
+               /* members[i][0] = members[i + 1][0];
                 members[i][1] = members[i + 1][1];
-                members[i][2] = members[i + 1][2];
+                members[i][2] = members[i + 1][2];*/
 
-                members[memberCnt][0] = null;
+
+                /*members[memberCnt][0] = null;
                 members[memberCnt][1] = null;
                 members[memberCnt][2] = null;
+                //회원 삭제 시 맴버수도 하나 감소시켜야 한다.
+                memberCnt--;
+                */
+                memberCnt = members.size();
                 break;
             }else{
                 System.out.println("해당 회원이 존재하지 않습니다.");
@@ -129,28 +144,35 @@ public class MemberMain {
 
     public static void main(String[] args) {
         int num = printPricePlan();
-        String[][] members = new String[num * 10][3];
+        List<Member> members = new ArrayList<>();
         totalCnt = num * 10;
         while(true){
             int choice = printMenu(memberCnt);
             switch (choice){
-                case 1 : addMember(members);
+                case 1 :
+                    addMember(members);
+                    break;
                 case 2 :
                     System.out.println("검색하실 이메일을 입력해주세요");
                     String email = sc.nextLine();
                     selectEmail(members, email);
+                    break;
                 case 3 :
                     System.out.println("검색하실 이름을 입력해주세요");
                     String name = sc.nextLine();
                     selectName(members, name);
+                    break;
                 case 4 :
                     selectAll(members);
+                    break;
                 case 5 :
                     System.out.println("변경하실 정보의 이메일을 입력해주세요");
                     updateMember(members, sc.nextLine());
+                    break;
                 case 6 :
                     System.out.println("삭제하실 이메일을 입력해주세요");
                     deleteMember(members, sc.nextLine());
+                    break;
                 case 7 :
                     System.out.println("이용해주셔서 감사합니다."); return;
                 default:
