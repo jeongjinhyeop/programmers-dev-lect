97946f8fd6d87612da3813399216535ecd6259a0
mac <mac@macui-MacBookPro.local>
2026-06-12 11:40:34 +0900

    MyLinkedlist 직접 만들기

--- java-part-3/src/main/java/list/MyArrayListTest.java
+++ java-part-3/src/main/java/list/MyArrayListTest.java
@@ -1,6 +1,8 @@
 package list;
 
-public class MyArrayList {
+import java.util.Arrays;
+
+public class A_collections_list_ex_array {
     public static void main(String[] args) {
         MyArrayList list = new MyArrayList();
 
@@ -14,7 +16,10 @@ public class MyArrayList {
                 + list.get(2));                // 기대: 가, 나, 다
 
         // --- Step 6 확인 ---        list.addFirst("앞");
-        System.out.println("addFirst 후 0,1 = " + list.get(0) + ", " + list.get(1)); // 기대: 앞, 가
+        list.addFirst("앞");
+        list.remove(2);
+        list.addLast("라");
+        System.out.println("addFirst 후 0,1 = " + list.get(0) + ", " + list.get(1) + ", " + list.get(list.size() - 1)); // 기대: 앞, 가, 라
         System.out.println("size = " + list.size());                 // 기대: 4
     }
 }
@@ -28,41 +33,54 @@ class MyArrayList {
 
     // [Step 2] 맨 뒤에 추가
     void addLast(String data) {
-        // TODO: 빈 끝자리(size 위치)에 data를 넣고, size를 1 늘리세요.
+        ensureCapacity();
+         arr[size] = data;
+         size++;
     }
 
     // [Step 3] 인덱스로 꺼내기
     String get(int index) {
-        // TODO: index 위치의 값을 반환하세요.
-        return null;
+
+        return arr[index];
     }
 
     // [Step 4] 현재 개수
     int size() {
-        // TODO: size를 반환하세요.
-        return 0;
+
+        return size;
     }
 
     // [Step 5] 공간이 꽉 찼으면 배열을 2배로 늘리기
     private void ensureCapacity() {
-        // TODO: size가 arr.length와 같으면
-        //       arr = Arrays.copyOf(arr, arr.length * 2); 로 교체하세요.
+        if(size == arr.length) arr = Arrays.copyOf(arr, arr.length * 2);
     }
 
     // [Step 6] 맨 앞에 추가  ★핵심★
     void addFirst(String data) {
-        // TODO: 1) ensureCapacity() 호출
-        //       2) for 문으로 맨 뒤에서부터 arr[i] = arr[i - 1] 한 칸씩 밀기
-        //       3) arr[0] = data;  그리고 size++
+        ensureCapacity();
+        for (int i = size; i > 0; i--) {
+            arr[i] = arr[i - 1];
+        }
+        arr[0] = data;
+        size++;
     }
 
     // [도전] index 위치에 삽입
     void insert(int index, String data) {
-        // TODO (도전): index 뒤의 요소들을 한 칸씩 밀고, 그 자리에 data를 넣으세요.
+        ensureCapacity();
+        for (int i = size; i > index; i--) {
+            arr[i] = arr[i - 1];
+        }
+        arr[index] = data;
+        size++;
     }
 
     // [도전] index 위치 삭제
     void remove(int index) {
-        // TODO (도전): index 뒤의 요소들을 한 칸씩 앞으로 당기고 size--
+        for (int i = index; i < size - 1; i++) {
+            arr[i] = arr[i + 1];
+        }
+        arr[size - 1] = null;
+        size--;
     }
 }
